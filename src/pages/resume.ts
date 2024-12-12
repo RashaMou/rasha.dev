f4a92f7c4d9bcb073780a386a7da356c442a112a
Rasha Moumneh <rasha@rasha.dev>
2024-12-12 10:12:30 -0500

    Add resume

---
+++ src/pages/resume.ts
@@ -0,0 +1,15 @@
+import type { APIRoute } from "astro";
+import * as fs from "fs";
+import * as path from "path";
+
+export const GET: APIRoute = async () => {
+  const filePath = path.resolve("./public/Rasha_Moumneh_Resume_2024.pdf");
+  const file = await fs.promises.readFile(filePath);
+
+  return new Response(file, {
+    headers: {
+      "Content-Type": "application/pdf",
+      "Content-Disposition": 'inline; filename="Rasha_Moumneh_Resume_2024.pdf"',
+    },
+  });
+};
